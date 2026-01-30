e4704ff8d4d306e475cf1a9232e9c3ac08039e9b
mikaborges <mika.oliveira1998@gmail.com>
2026-01-30 10:09:30 +0000

    Modificando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home () {
-    return <h1>Oi amor. Quero que saiba que você é tudo para mim ... Obrigada por tudo. Te amo muito!!!</h1>
+    return <h1>Oi minha vida. Quero que saiba que você é tudo para mim ... Obrigada por tudo. Te amo muito!!!!</h1>
 }
 
 export default Home;
